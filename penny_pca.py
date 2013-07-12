bdf14a7f3e07a230757e87a8ba1f7911025ab4f7
Nick Timkovich <prometheus235@gmail.com>
2013-07-12 00:33:46 -0500

    PCA "works" (I think), but don't quite know how to turn the result into something visual...

--- penny_pca.py
+++ penny_pca.py
@@ -8,3 +8,33 @@ Does principal component analysis on the penny colors.  Ordinarly the
 principal components are some totally abstract thing, but because we're 
 working with colors, the axes are also colors, easily(?) visualizable.
 '''
+
+import sys
+import argparse
+
+import numpy as np
+#import matplotlib.pyplot as plt
+
+import penny_reconstructor as penny_r
+import pca
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv[1:]
+
+    parser = argparse.ArgumentParser(description='Does principal component '
+        'analysis on the penny colors')
+    parser.add_argument('image', type=str, help='Image filename used to '
+        'generate histograms (not actually read)')
+    args = parser.parse_args(argv)
+
+    histogram_file_in = args.image + '_hist.csv'
+    colors = np.array(penny_r.get_colors(histogram_file_in))
+
+    p = pca.PCA(colors, fraction=1)
+
+    import pdb;pdb.set_trace()
+    print p
+
+if __name__ == '__main__':
+    sys.exit(main())
